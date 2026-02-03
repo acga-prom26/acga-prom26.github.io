a40060c0da5d45c851ce0196ea789b81fe5caf49
Ivan Sadílek <sadilekivan@gmail.com>
2026-02-03 16:56:34 +0100

    background improvement, some nix build attempts

--- flake.nix
+++ flake.nix
@@ -10,6 +10,22 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
+      packages.${system}.default = pkgs.buildNpmPackage {
+        pname = "eleventy-site";
+        version = "1.0.0";
+        src = ./.;
+
+        # 1. Run 'nix build'
+        # 2. It will fail and give you a hash
+        # 3. Replace this string with that hash
+        npmDepsHash = "sha256-ku9oVBR9A6ex6oBrFybJCWehzUq3a/K83vsriikdfDw=";
+
+        installPhase = ''
+          npx @11ty/eleventy
+          # cp -r _site $out
+        '';
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
           pkgs.nodejs_20
